49aebd60f8369e9c629283d3e3794a2ff8f8f489
Jeremy Taylor <jeremy.taylor92@gmail.com>
2021-02-13 16:10:22 -0500

    All progress

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Jeremy Taylor',
+    username: 'JermsTheCasual',
+    email: 'jeremy@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Bob Downes',
+    username: 'Bob',
+    email: 'bob@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Jim Cowell',
+    username: 'Jim',
+    email: 'jim@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Dawn Lo',
+    username: 'Dawn',
+    email: 'dawn@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Bill Murray',
+    username: 'Bill',
+    email: 'bill@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Jessica Hill',
+    username: 'Jessica',
+    email: 'jessica@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Baxter Lander',
+    username: 'Baxter',
+    email: 'baxter@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Doug Funny',
+    username: 'Doug',
+    email: 'doug@gmail.com'
+  }
+]
